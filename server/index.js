ed4950ce7b9eaa237c778856bf980400ee459f10
Deepak Sharma <ds9999106554@gmail.com>
2024-02-06 23:46:59 +0530

    remove razorpay

--- server/index.js
+++ server/index.js
@@ -25,10 +25,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
-		credentials:true,
+		origin: "*",
+		credentials: true,
 	})
-)
+);
 
 app.use(
 	fileUpload({
